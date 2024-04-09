fea4a05a0dff5c3d968ee0ad4019bad1ceb4bbff
DeanNguyen98 <nctuan.hn@gmail.com>
2024-04-09 19:13:30 +1000

    add Event Listener for task logic

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
-import { createNewProject , getActiveProject } from "./Project";
-
+import { createNewProject , getActiveProject, renderProject,projectlist } from "./Project";
+import { closeTaskModal, openTaskModal, addNewTask } from "./Task";
 function ProjectEventListener () {
     const projectform = document.querySelector(".project-form");
     projectform.addEventListener("submit", (createNewProject));
     const navbar = document.querySelector(".navbar");
     navbar.addEventListener("click", (getActiveProject));
+    renderProject(projectlist);
 }
 
+function TaskEventListener() {
+    const taskform = document.querySelector("[data-new-task-form]");
+    taskform.addEventListener("submit", (addNewTask));
+}
 ProjectEventListener();
+TaskEventListener();
+openTaskModal();
+closeTaskModal();
 
