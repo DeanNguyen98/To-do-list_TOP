da974515e0eeb9f2403467de9a02eb7b996a6e3e
DeanNguyen98 <nctuan.hn@gmail.com>
2024-04-12 23:20:03 +1000

    Add condition for task delete button to only works if activeId is not All and Today

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import { createNewProject , getActiveProject, renderProject,projectlist } from "./Project";
+import { createNewProject , getActiveProject, renderProject,projectlist,activeId } from "./Project";
 import { closeTaskModal, openTaskModal, addNewTask, removeCompletedTask } from "./Task";
+import { removeTaskinAll } from "./navbar";
 function ProjectEventListener () {
     const projectform = document.querySelector(".project-form");
     projectform.addEventListener("submit", (createNewProject));
@@ -12,7 +13,12 @@ function TaskEventListener() {
     const taskform = document.querySelector("[data-new-task-form]");
     taskform.addEventListener("submit", (addNewTask));
     const taskDelBtn = document.querySelector(".task-del");
-    taskDelBtn.addEventListener("click", (removeCompletedTask));
+    taskDelBtn.addEventListener("click", function() {
+        if (activeId !== "All" && activeId !== "Today") {
+            removeCompletedTask();
+        }
+    });
+
 }
 ProjectEventListener();
 TaskEventListener();
